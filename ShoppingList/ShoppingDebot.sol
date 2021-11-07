9a957f68e2102719c29a4598c7b7ab4c9c6511e2
Artem Sadovskiy <artem8sadovskij@gmail.com>
2021-11-08 00:23:02 +0300

    rename ShoppingDebot

--- ShoppingList/ShoppingDebot.sol
+++ ShoppingList/ShoppingDebot.sol
@@ -10,13 +10,13 @@ contract ShoppingDebot is AShoppingListDebot{
         string name, string version, string publisher, string key, string author,
         address support, string hello, string language, string dabi, bytes icon
     ) {
-        name = "Filling Shopping List DeBot";
+        name = "Shoppin Debot DeBot";
         version = "0.1.0";
         publisher = "";
-        key = "Filling Shopping List manager";
+        key = "Shoppin Debot manager";
         author = "";
         support = address.makeAddrStd(0, 0x66e01d6df5a8d7677d9ab2daf7f258f1e2a7fe73da5320300395f99e01dc3b5f);
-        hello = "Hi, i'm a Filling Shopping List DeBot.";
+        hello = "Hi, i'm a Shopping Debot.";
         language = "en";
         dabi = m_debotAbi.get();
     }
